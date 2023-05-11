5bddc521638e2f7e9de0321fab971b2c3f240ded
ShiraReisner <117013954+ShiraReisner@users.noreply.github.com>
2023-05-11 19:57:51 +0300

    CoronaManagement.BL

---
+++ IPatientBL.cs
@@ -0,0 +1,15 @@
+﻿using CoronaManagement.Models;
+using Entities;
+
+namespace CoronaManagement.BL
+{
+    public interface IPatientBL
+    {
+        List<PersonalDetail> GetDetails();
+        int PostPatient(PersonalDetail personalDetail);
+        int ActivePatients(DateTime day);
+        int NotVaccinated();
+         Task<int> PostImg(Picture picture, int id);
+        //Task GetPatientById(int id);
+    }
+}
---
+++ PatientBL.cs
@@ -0,0 +1,57 @@
+﻿using CoronaManagement.DAL;
+using CoronaManagement.Models;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronaManagement.BL
+{
+    public class PatientBL : IPatientBL
+    {
+        private IPatientDAL _patientDAL;
+
+        public PatientBL(IPatientDAL patientDAL)
+        {
+            _patientDAL = patientDAL;
+        }
+        public List<PersonalDetail> GetDetails()
+        {
+            return _patientDAL.GetAllPatients();
+        }
+        public int PostPatient(PersonalDetail personalDetail)
+        {
+            return _patientDAL.PostPatient(personalDetail);
+        }
+        public int ActivePatients(DateTime day)
+        {
+            return _patientDAL.ActivePatients(day);
+        }
+        public int NotVaccinated()
+        {
+            return _patientDAL.NotVaccinated();
+        }
+        public async Task<int> PostImg(Picture picture, int id)
+        {
+            IFormFile file = picture.File;
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    Byte[] fileBytes = ms.ToArray();
+                    return await _patientDAL.PostImg(fileBytes, id);
+                }
+            }
+            return -1;
+        }
+
+        //public Task GetPatientById(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
